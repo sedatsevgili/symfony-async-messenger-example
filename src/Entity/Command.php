52d478089fb2e66daebf94b53149d87420b6f635
Sedat Sevgili <sedatsevgili@gmail.com>
2021-10-24 16:02:24 +0300

    install doctrine

---
+++ src/Entity/Command.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommandRepository::class)
+ */
+class Command
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $command;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $result;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $runnedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCommand(): ?string
+    {
+        return $this->command;
+    }
+
+    public function setCommand(string $command): self
+    {
+        $this->command = $command;
+
+        return $this;
+    }
+
+    public function getResult(): ?string
+    {
+        return $this->result;
+    }
+
+    public function setResult(?string $result): self
+    {
+        $this->result = $result;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getRunnedAt(): ?\DateTimeImmutable
+    {
+        return $this->runnedAt;
+    }
+
+    public function setRunnedAt(\DateTimeImmutable $runnedAt): self
+    {
+        $this->runnedAt = $runnedAt;
+
+        return $this;
+    }
+}
